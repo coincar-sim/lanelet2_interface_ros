ac7740f73114f41826f15abbe691dfd45a6397c6
Maximilian Naumann <maximilian.naumann@kit.edu>
2019-01-10 15:38:44 +0100

    Rework python implementation: use rospy in order to avoid signal handler issues between rospy and roscpp in rospy nodes using this library (before, a roscpp node was initialized, taking the signal away from the rospy node, as a roscpp nodehandle cannot access the rospy node) closes #3

--- include/lanelet2_interface_ros/lanelet2_interface_ros.hpp
+++ include/lanelet2_interface_ros/lanelet2_interface_ros.hpp
@@ -10,20 +10,23 @@ namespace lanelet2_interface_ros {
 class Lanelet2InterfaceRos {
 public:
     Lanelet2InterfaceRos() = default;
+    virtual ~Lanelet2InterfaceRos() = default;
     std::string waitForFrameIdMap(double pollRateHz = 10, double timeOutSecs = 30);
     lanelet::LaneletMapConstPtr waitForMapPtr(double pollRateHz = 10, double timeOutSecs = 30);
     lanelet::LaneletMapPtr waitForNonConstMapPtr(double pollRateHz = 10, double timeOutSecs = 30);
     std::shared_ptr<lanelet::Projector> waitForProjectorPtr(double pollRateHz = 10, double timeOutSecs = 30);
 
-private:
-    void waitForParams(double pollRateHz, double timeOutSecs);
-    void loadMap();
+protected:
+    virtual void waitForParams(double pollRateHz, double timeOutSecs);
     struct InterfaceParams {
         bool frameIdMapFound{false}, mapFileNameFound{false}, latOriginFound{false}, lonOriginFound{false};
         double latOrigin{0.}, lonOrigin{0.};
         std::string mapFileName{""}, frameIdMap{""};
     };
     InterfaceParams params_;
+
+private:
+    void loadMap();
 
     std::string frameIdMap_;
     lanelet::LaneletMapPtr nonConstMapPtr_;
